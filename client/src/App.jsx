dabd2efdff4a5d7b6020b9409603572f1cf1cd00
dankukennedy <dankukennedy@email.com>
2024-02-26 12:55:03 +0000

    create a header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,20 @@ import About from './pages/About'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
-import Project from './pages/Project'
+import Projects from './pages/Projects'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+     <Header/>
       <Routes>
          <Route path="/" element ={<Home />}/>
          <Route path="/about" element ={<About />}/>
          <Route path="/sign-in" element ={<Signin />}/>
          <Route path="/sign-up" element ={<Signup />}/>             
          <Route path="/dashboard" element ={<Dashboard />}/>             
-         <Route path="/project" element ={<Project />}/>             
+         <Route path="/projects" element ={<Projects />}/>             
       </Routes>
     
     </BrowserRouter>
